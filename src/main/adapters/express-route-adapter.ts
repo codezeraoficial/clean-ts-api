121c340630c1d0da39481789935699d079433f4f
Leonardo Viana <codezeraoficial@gmail.com>
2020-10-01 17:57:20 -0300

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+import { IController, HttpRequest } from '../../presentations/protocols'
+
+export const adaptRoute = (controller: IController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
