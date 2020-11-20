1237340b5d4b73e45b9e31f838483747beaf11db
Leonardo Viana <codezeraoficial@gmail.com>
2020-11-20 17:01:06 -0300

    feat: ensure SurveyMongoRepository add a new survey on add success

--- src/data/protocols/db/survey/add-survey-repository.ts
+++ src/data/protocols/db/survey/add-survey-repository.ts
@@ -1,6 +1,5 @@
-import { AccountModel } from '../../../../domain/models/account'
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
 
 export interface AddSurveyRepository{
-  add: (account: AddSurveyModel) => Promise<AccountModel>
+  add: (data: AddSurveyModel) => Promise<void>
 }
