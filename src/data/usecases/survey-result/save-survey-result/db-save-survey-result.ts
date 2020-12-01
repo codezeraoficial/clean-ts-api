5386b37241df8149efea87f805d17e1c884cbc5d
Leonardo Viana <codezeraoficial@gmail.com>
2020-12-01 19:32:32 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
@@ -1,6 +1,6 @@
-import { SaveSurveyresult, SaveSurveyResultModel, SaveSurveyResultRepository, SurveyResultModel } from './db-save-survey-result-protocols'
+import { SaveSurveyResult, SaveSurveyResultModel, SaveSurveyResultRepository, SurveyResultModel } from './db-save-survey-result-protocols'
 
-export class DbSaveSurveyResult implements SaveSurveyresult {
+export class DbSaveSurveyResult implements SaveSurveyResult {
   constructor (private readonly saveSurveyResultRepository: SaveSurveyResultRepository) {}
 
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
