77b65df61d38d79ab1275558d4645bd3fa734053
Leonardo Viana <codezeraoficial@gmail.com>
2020-12-01 19:40:03 -0300

    test: ensure SaveSurveyResultController retunrs 500 if SaveSurveyResult throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -123,4 +123,11 @@ describe('SaveSurveyResult Controlller', () => {
       answer: 'any_answer'
     })
   })
+
+  test('Should retur 500 if SaveSurveyResult throws', async () => {
+    const { sut, saveSurveyResultStub } = makeSut()
+    jest.spyOn(saveSurveyResultStub, 'save').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
