5c1bd91d5e365b18837dc810d1b490eee0cf24fb
Leonardo Viana <codezeraoficial@gmail.com>
2020-12-07 19:31:15 -0300

    docs: add login api docs

---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,27 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'API para auteticar usuário',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Sucess',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
