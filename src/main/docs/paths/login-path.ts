5e35bcd88da3a6ea0e898baf188f45abc0a63680
Leonardo Viana <codezeraoficial@gmail.com>
2020-12-07 20:01:55 -0300

    docs: update login api docs

--- src/main/docs/paths/login-path.ts
+++ src/main/docs/paths/login-path.ts
@@ -21,6 +21,18 @@ export const loginPath = {
             }
           }
         }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      401: {
+        $ref: '#/components/unauthorized'
+      },
+      500: {
+        $ref: '#/components/serverError'
       }
     }
   }
